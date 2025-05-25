e1faabb725202b07756fde461c2db813a753a84f
s0594520 <Ahmadbaz.Chedo@Student.HTW-Berlin.de>
2025-05-26 00:54:04 +0200

    test4\

--- BooknestFrontend/src/views/AboutView.vue
+++ BooknestFrontend/src/views/AboutView.vue
@@ -15,17 +15,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import axios from 'axios'
+import { defineComponent } from 'vue';
 
-const baseURL = import.meta.env.VITE_APP_BACKEND_BASE_URL
-const apiEndpoint = `${baseURL}/books`
+const baseURL = import.meta.env.VITE_APP_BACKEND_BASE_URL;
+const apiEndpoint = baseURL + '/books'
 
 interface Book {
-  id: number
-  title: string
-  author: string
-  genre: string
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
 }
 
 export default defineComponent({
@@ -33,26 +32,23 @@ export default defineComponent({
   data() {
     return {
       books: [] as Book[],
-    }
-  },
-  methods: {
-    requestBooks(): void {
-      axios
-        .get<Book[]>(apiEndpoint)
-        .then((res) => {
-          console.log(res.data)
-          this.books = res.data
-        })
-        .catch((error) => {
-          this.logError(error)
-        })
-    },
-    logError(error: unknown) {
-      console.error('Fehler beim Abrufen der Bücher:', error)
-    },
+    };
   },
   mounted() {
-    this.requestBooks()
+    fetch(apiEndpoint)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Fehler beim Laden: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.books = data;
+      })
+      .catch(error => {
+        console.error("Fehler beim Abrufen der Bücher:", error);
+      });
   },
-})
+});
+
 </script>
