6951256e390ef7c92982a6bc7cdf724537fe086c
s0594520 <Ahmadbaz.Chedo@Student.HTW-Berlin.de>
2025-05-25 23:35:53 +0200

    comm1

--- BooknestFrontend/src/views/AboutView.vue
+++ BooknestFrontend/src/views/AboutView.vue
@@ -17,6 +17,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+const baseURL = import.meta.env.VITE_APP_BACKEND_BASE_URL;
+const apiEndpoint = `${baseURL}/books`;
+
 interface Book {
   id: number;
   title: string;
@@ -27,70 +30,24 @@ interface Book {
 export default defineComponent({
   name: 'BookListView',
   data() {
-
     return {
-      books: [
-        { id: 1, title: 'Der Herr der Ringe', author: 'J.R.R. Tolkien', genre: 'Fantasy' },
-        { id: 2, title: '1984', author: 'George Orwell', genre: 'Dystopie' },
-        { id: 3, title: 'Die Unendliche Geschichte', author: 'Michael Ende', genre: 'Fantasy' },
-      ] as Book[], 
+      books: [] as Book[],
     };
+  },
+  mounted() {
+    fetch(apiEndpoint)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Fehler beim Laden: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.books = data;
+      })
+      .catch(error => {
+        console.error("Fehler beim Abrufen der Bücher:", error);
+      });
   },
 });
 </script>
-
-<style scoped>
-.book-list-view {
-  font-family: Arial, sans-serif;
-  padding: 20px;
-}
-
-h2 {
-  text-align: center;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-.book-item {
-  border: 1px solid #ddd;
-  padding: 15px;
-  margin: 10px 0;
-  border-radius: 5px;
-  background-color: #f9f9f9;
-  transition: transform 0.3s ease, box-shadow 0.3s ease;
-}
-
-.book-item h3 {
-  margin: 0 0 10px 0;
-  color: black;  /* Setzt den Text auf schwarz */
-  background-color: white;  /* Weißer Hintergrund */
-  padding: 5px;  /* Etwas Padding, um den Text vom Rand zu trennen */
-  border-radius: 5px;  /* Abgerundete Ecken */
-}
-
-
-.book-item p {
- color: black;  /* Setzt den Text auf schwarz */
-  background-color: white;  /* Weißer Hintergrund */
-  padding: 5px;  /* Etwas Padding, um den Text vom Rand zu trennen */
-  border-radius: 5px;  /* Abgerundete Ecken */
-}
-
-.book-item:hover {
-  transform: translateY(-5px);
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
-
-.no-books {
-  text-align: center;
-  font-size: 18px;
-  color: #555;
-  padding: 20px;
-  border: 1px solid #ddd;
-  background-color: #f9f9f9;
-  border-radius: 5px;
-}
-</style>
